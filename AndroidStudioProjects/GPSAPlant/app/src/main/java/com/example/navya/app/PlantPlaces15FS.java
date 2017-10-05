7619d344de274e3463af5a6bb00f48192713e3d7
Navya <navyabgrao@gmail.com>
2017-10-05 17:19:33 +0530

    first commit

---
+++ AndroidStudioProjects/GPSAPlant/app/src/main/java/com/example/navya/app/PlantPlaces15FS.java
@@ -0,0 +1,13 @@
+package com.example.navya.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantPlaces15FS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_places15_fs);
+    }
+}
---
+++ AndroidStudioProjects/tripler001/app/src/main/java/com/example/navya/tripler001/PlaceholderFragment.java
@@ -0,0 +1,8 @@
+package com.example.navya.tripler001;
+
+/**
+ * Created by Navya on 10-07-2017.
+ */
+
+class PlaceholderFragment {
+}
